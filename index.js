428f07ec4877ba3a2b722915a8c2614a2f5f2690
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-18 16:15:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-let driver = {};
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  newDriver = Object.assign({}, driver);
+  newDriver[key] = value;
+  return newDriver;
+}
